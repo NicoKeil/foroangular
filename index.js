015db410263ed15f700b931fca4e963753827fd2
NicoKeil <70778470+NicoKeil@users.noreply.github.com>
2020-10-03 12:44:54 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3900;
+var port = process.env.PORT || 3999;
 
 mongoose.set('useFindAndModify', false)
 		.set('useUnifiedTopology', true);
